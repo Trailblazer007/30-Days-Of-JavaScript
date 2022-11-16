50844322c7c9d8a63e7dd8735846aafa6a77182c
Fatoye Oluwaseun <fatoyeoluwaseun@gmail.com>
2022-11-16 18:32:50 +0100

    Started ex. level 3, no. 1: sorted the countries by name

--- 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
@@ -1,2 +1,13 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+import { countries } from "../data/countries_data.js";
+
+console.log(countries);
+
+// sorting the countries by name
+let sortedByName = countries.sort((a, b) => {
+  return a.name - b.name;
+});
+console.log(sortedByName);
+
+// sorting the countries by capital
+
+// sorting the countries by population
