8201fb3addf035be827188f41f22b145c23ef8ba
Fatoye Oluwaseun <fatoyeoluwaseun@gmail.com>
2022-11-26 13:58:28 +0100

    solved Ex. lvl 3, no. 2

--- 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
@@ -15,3 +15,52 @@ import { countries } from "../data/countries_data.js";
 // sorting the countries by population
 // let sortedByPopulation = countries.sort((a, b) => a.population - b.population);
 // console.log(sortedByPopulation);
+
+// 2. Find the 10 most spoken languages
+
+// using recursion
+// const mostSpokenLanguages = (arr, number) => {
+//   let holder = [];
+//   let answer = [];
+
+//   for (const { languages } of arr) holder.push(languages);
+
+//   function sorter(newArr) {
+//     if (!newArr.length) return;
+
+//     let lengthOfLang = newArr.filter((each) => each === newArr[0]).length;
+//     let rest = newArr.filter((each) => each != newArr[0]);
+
+//     answer.push({ country: newArr[0], count: lengthOfLang });
+//     sorter(rest);
+//   }
+//   sorter(holder.flat());
+
+//   answer.sort((a, b) => b.count - a.count);
+//   return answer.slice(0, number);
+// };
+// console.log(mostSpokenLanguages(countries, 10));
+
+// using another method
+
+// const mostSpokenLanguages = (arr, number) => {
+//   let holder = [];
+//   let result = [];
+
+//   for (let { languages } of arr) {
+//     holder.push(languages);
+//   }
+
+//   holder = holder.flat();
+
+//   let answer = holder.reduce((acc, cv) => {
+//     !acc[cv] ? (acc[cv] = 1) : acc[cv]++;
+//     return acc;
+//   }, {});
+
+//   for (let each in answer) {
+//     result.push({ language: each, count: answer[each] });
+//   }
+//   return result.sort((a, b) => b.count - a.count).slice(0, number);
+// };
+// console.log(mostSpokenLanguages(countries, 10));
