c0ff98b9ee261a83c1d509465e943a29a92fea22
Fatoye Oluwaseun <fatoyeoluwaseun@gmail.com>
2023-02-11 13:52:17 +0100

    solved most of the tasks under this topic

--- 19_Day_Closures/19_day_starter/scripts/main.js
+++ 19_Day_Closures/19_day_starter/scripts/main.js
@@ -1,2 +1,27 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// Exercises
+// Exercises: Level 1
+// 1. Create a closure which has one inner function
+function sum(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+console.log(sum(1)(2));
+
+// Exercises: Level 2
+// 1. Create a closure which has three inner functions
+function multiply(a) {
+  return function (b) {
+    return function (c) {
+      return b * c * a;
+    };
+  };
+}
+
+console.log(multiply(2)(3)(4));
+
+// Exercises: Level 3
+// 1. Create a personAccount out function. It has firstname, lastname, incomes, expenses inner variables.
+// It has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance inner functions.
+// Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
